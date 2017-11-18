00e84f0e84f664ea3b991d84e0d342ebb7393116
Lukas Holecek <hluk@email.cz>
2017-11-18 12:30:07 +0100

    Allow to set tray and window icon tag
    Adds iconTag() and iconTagColor() scriptables.
    
    Allows to set default values using COPYQ_SESSION_TAG and
    COPYQ_SESSION_TAG_COLOR.

--- src/scriptable/scriptableproxy.h
+++ src/scriptable/scriptableproxy.h
@@ -193,6 +193,12 @@ public:
     QString iconColor();
     bool setIconColor(const QString &name);
 
+    QString iconTag();
+    void setIconTag(const QString &tag);
+
+    QString iconTagColor();
+    bool setIconTagColor(const QString &name);
+
 private:
     ClipboardBrowser *fetchBrowser(const QString &tabName);
     ClipboardBrowser *fetchBrowser();
